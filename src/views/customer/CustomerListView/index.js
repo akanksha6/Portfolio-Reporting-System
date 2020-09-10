a84609a78dac9b55cf45e0d928146e7b4c350cda
SanjanaK703 <71081646+SanjanaK703@users.noreply.github.com>
2020-09-10 19:16:21 +0530

    Update index.js

--- src/views/customer/CustomerListView/index.js
+++ src/views/customer/CustomerListView/index.js
@@ -18,6 +18,15 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}));
+
 const CustomerListView = () => {
   const classes = useStyles();
   const [customers] = useState(data);
