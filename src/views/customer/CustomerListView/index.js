b59403f28410c75cf24746bd325f38b8fc1900e9
MahendraPrajapati42 <43578017+MahendraPrajapati42@users.noreply.github.com>
2020-09-11 14:20:51 +0530

    update done

--- src/views/customer/CustomerListView/index.js
+++ src/views/customer/CustomerListView/index.js
@@ -18,14 +18,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.dark,
-    minHeight: '100%',
-    paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(3)
-  }
-}));
+
 
 const CustomerListView = () => {
   const classes = useStyles();
--- src/views/reports/DashboardView/index.js
+++ src/views/reports/DashboardView/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {
+  Box,
   Container,
   Grid,
-  makeStyles
+  makeStyles,
+  Button
 } from '@material-ui/core';
 import Page from 'src/components/Page';
 import Budget from './Budget';
@@ -21,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
     minHeight: '100%',
     paddingBottom: theme.spacing(3),
     paddingTop: theme.spacing(3)
-  }
+  },
   importButton: {
     marginRight: theme.spacing(3),
     paddingBottom: theme.spacing(2),
@@ -38,104 +40,85 @@ const Dashboard = () => {
   const classes = useStyles();
 
   return (
-    <Page
-      className={classes.root}
-      title="Dashboard"
-    >
+    <div>
+    <Page  className={classes.root} title="Dashboard" >
        <Container maxWidth={false}>
-   
-          <Box
-          display="flex"
-          justifyContent="flex-end"
-          >
-            <Button className={classes.importButton}>
-              Import
-             </Button>
-            <Button className={classes.exportButton}>
-              Export
-            </Button>
-            <Button
-              color="primary"
-              variant="contained"
+  
+        <Box
+            display="flex"
+            justifyContent="flex-end"
             >
-              Add customer
-            </Button>
+              <Button className={classes.importButton}>
+                Import
+              </Button>
+              <Button className={classes.exportButton}>
+                Export
+              </Button>
+              <Button
+                color="primary"
+                variant="contained"
+              >
+                Add customer
+              </Button>
         </Box>
         <Box mt={3}>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
+          <Grid container
+            spacing={3}
           >
-            <Budget />
-          </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TotalCustomers />
-          </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TasksProgress />
-          </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TotalProfit />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <Sales />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <TrafficByDevice />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-	     <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <Line />
-          </Grid>
+            <Grid
+              item
+              lg={3}
+              sm={6}
+              xl={3}
+              xs={12}
+             >
+              <Budget />
+            </Grid>
+            <Grid
+              item
+              lg={3}
+              sm={6}
+              xl={3}
+              xs={12}
+            >
+              <TotalCustomers />
+            </Grid>
+            <Grid
+              item
+              lg={3}
+              sm={6}
+              xl={3}
+              xs={12}
+            >
+              <TasksProgress />
+            </Grid>
+            <Grid
+              item
+              lg={3}
+              sm={6}
+              xl={3}
+              xs={12}
+            >
+              <TotalProfit />
+            </Grid>
+            <Grid
+              item
+              lg={8}
+              md={12}
+              xl={9}
+              xs={12}
+            >
+              <Sales />
+            </Grid>
+            <Grid
+              item
+              lg={4}
+              md={6}
+              xl={3}
+              xs={12}
+            >
+              <TrafficByDevice />
+            </Grid>
           <Grid
             item
             lg={4}
@@ -143,21 +126,39 @@ const Dashboard = () => {
             xl={3}
             xs={12}
           >
-            <LatestProducts />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <LatestOrders />
+            <Grid
+                item
+                lg={8}
+                md={12}
+                xl={9}
+                xs={12}
+              >
+                <Line />
+              </Grid>
+            <Grid
+              item
+              lg={4}
+              md={6}
+              xl={3}
+              xs={12}
+            >
+              <LatestProducts />
+            </Grid>
+            <Grid
+              item
+              lg={8}
+              md={12}
+              xl={9}
+              xs={12}
+            >
+              <LatestOrders />
+            </Grid>
           </Grid>
           </Grid>
           </Box>
       </Container>
     </Page>
+    </div>
   );
 };
 
